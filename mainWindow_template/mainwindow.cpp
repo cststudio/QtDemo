df4dd0a40bf9e4524457722c955a34fd560b96a9
Late Lee <li@latelee.org>
2020-01-20 22:23:46 +0800

    补充gif文件。 提交一个空的工程模板。

---
+++ mainWindow_template/mainwindow.cpp
@@ -0,0 +1,154 @@
+﻿#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    initMainWindow();
+
+    initWindow();
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+// 对主窗口的初始化
+void MainWindow::initMainWindow()
+{
+    setWindowTitle(tr("MainWindow Demo"));
+    setMinimumSize(480, 320);
+
+    Qt::WindowFlags winFlags  = Qt::Dialog;
+    winFlags = winFlags | Qt::WindowMinMaxButtonsHint | Qt::WindowCloseButtonHint;
+
+    setWindowFlags(winFlags);
+}
+
+// 布局、控件、信号等初始化
+void MainWindow::initWindow()
+{
+    m_pressMouse = 0;
+    m_stsEmpty = nullptr;
+    m_stsDebugInfo = nullptr;
+    m_stsSysTime = nullptr;
+    m_stsCopyright = nullptr;
+
+    initStatusBar();
+}
+
+void MainWindow::initStatusBar()
+{
+    // 状态栏分别为：
+    // 临时信息（可不用）做一空的label占位
+    // 提示信息（可多个）
+    // 系统时间
+    // 版本信息（或版权声明）
+
+    //ui->statusbar->setStyleSheet(QString("QStatusBar::item{border: 0px}")); // 不显示边框
+    ui->statusbar->setSizeGripEnabled(false);//去掉状态栏右下角的三角
+
+
+    m_stsEmpty = new QLabel();
+    m_stsDebugInfo = new QLabel();
+    m_stsSysTime = new QLabel();
+    m_stsCopyright = new QLabel();
+    m_stsExit = new QLabel();
+
+    m_stsEmpty->setMinimumWidth(100);
+    ui->statusbar->addWidget(m_stsEmpty);
+    m_stsDebugInfo->setMinimumWidth(this->width()/3);
+    ui->statusbar->addWidget(m_stsDebugInfo);
+
+    //ui->statusbar->showMessage(tr("临时信息!"),2000);//显示临时信息2000ms 前面的正常信息被覆盖 当去掉后一项时，会一直显示
+
+    QDateTime dateTime(QDateTime::currentDateTime());
+    QString timeStr = dateTime.toString("yyyy-MM-dd HH:mm:ss.zzz");
+    m_stsSysTime->setText(timeStr);
+    ui->statusbar->addPermanentWidget(m_stsSysTime);
+
+    // 版权信息
+    m_stsCopyright->setFrameStyle(QFrame::NoFrame);
+    m_stsCopyright->setText(tr("  <a href=\"https://www.latelee.org\">技术主页</a>  "));
+    m_stsCopyright->setOpenExternalLinks(true);
+    ui->statusbar->addPermanentWidget(m_stsCopyright);
+
+    // 退出图标
+    m_stsExit->installEventFilter(this); // 安装事件过滤，以便获取其单击事件
+    m_stsExit->setToolTip("Exit App");
+    // 贴图
+    QPixmap exitIcon(":/images/exit.jpg");
+    m_stsExit->setMinimumWidth(32);
+    m_stsExit->setPixmap(exitIcon);
+    ui->statusbar->addPermanentWidget(m_stsExit);
+    
+    connect(this, &MainWindow::sig_exit, qApp, &QApplication::quit); // 直接关联到全局的退出槽
+}
+
+bool MainWindow::eventFilter(QObject *watched, QEvent *event)
+{
+    if(watched == m_stsExit) // 程序退出
+    {
+        //判断事件
+        if(event->type() == QEvent::MouseButtonPress)
+        {
+            m_stsDebugInfo->setText("press exit label");
+            // TODO：直接退出还是发信号？
+            emit sig_exit();
+            return true; // 事件处理完毕
+        }
+        else
+        {
+            return false;
+        }
+    }
+    else
+    {
+        return QWidget::eventFilter(watched, event);
+    }
+}
+
+void MainWindow::mousePressEvent(QMouseEvent *event)
+{
+    //当鼠标左键点击时.
+    if (event->button() == Qt::LeftButton)
+    {
+        //记录鼠标的世界坐标
+        m_startPos = event->globalPos();
+        //记录窗体的世界坐标
+        m_windowPos = this->frameGeometry().topLeft();
+        m_pressMouse = 1;
+    }
+    else if(event->button() == Qt::RightButton)
+    {
+
+    }
+}
+
+void MainWindow::mouseMoveEvent(QMouseEvent *event)
+{
+    if (event->buttons() == Qt::LeftButton)
+    {
+        if (m_pressMouse)
+        {
+        //移动中的鼠标位置相对于初始位置的相对位置
+        QPoint relativePos = event->globalPos() - m_startPos;
+        //然后移动窗体即可
+        this->move(m_windowPos + relativePos );
+
+        }
+    }
+    else if(event->button() == Qt::RightButton)
+    {
+
+    }
+}
+
+void MainWindow::mouseReleaseEvent(QMouseEvent *event)
+{
+    m_pressMouse = 0;
+}
---
+++ mainWindow_template/mainwindow.h
@@ -0,0 +1,68 @@
+﻿#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QMouseEvent>
+#include <QKeyEvent>
+#include <QMessageBox>
+#include <QLabel>
+
+#include <QDateTime>
+#include <QDebug>
+
+#include <QSettings>
+#include <QFileInfo>
+#include <QStringLiteral>
+
+#include <QSystemTrayIcon>
+#include <QMenu>
+
+// 界面显示中文，文件需要使用UTF8 BOM格式保存，否则乱码--windows vs编译器测试
+#pragma execution_character_set("utf-8")
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+public:
+
+    // 各种初始化
+    void initMainWindow();
+    void initWindow();
+    void initStatusBar();
+
+    void mousePressEvent(QMouseEvent *event); // 鼠标按下
+    void mouseMoveEvent(QMouseEvent *event); // 鼠标移动
+    void mouseReleaseEvent(QMouseEvent *event); // 鼠标释放
+
+    int m_pressMouse; // 当点击下拉框时，窗口跳动，加上判断
+    QPoint m_startPos;
+    QPoint m_windowPos;
+
+    bool eventFilter(QObject *watched, QEvent *event);
+
+    // 状态栏相关
+    QLabel* m_stsEmpty;
+    QLabel* m_stsDebugInfo;
+    QLabel* m_stsSysTime;
+    QLabel* m_stsCopyright;
+    QLabel* m_stsExit;
+
+signals:
+    void sig_exit();
+
+private slots:
+
+private:
+    Ui::MainWindow *ui;
+
+};
+#endif // MAINWINDOW_H
